09a9e3fae32d27bc6558ea22a07ceb941af838b8
Darran Kelinske <darran.kelinske@gmail.com>
2019-01-18 20:43:51 -0600

    begin setting up chat head implementation

--- takehome/app/src/main/java/com/fivestars/takehome/MainActivity.java
+++ takehome/app/src/main/java/com/fivestars/takehome/MainActivity.java
@@ -19,14 +19,22 @@
 
 package com.fivestars.takehome;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import org.apache.cordova.*;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends CordovaActivity
-{
+import org.apache.cordova.CordovaActivity;
+
+public class MainActivity extends CordovaActivity {
+
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // enable Cordova apps to be started in the background
@@ -37,5 +45,49 @@ public class MainActivity extends CordovaActivity
 
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            initializeView();
+        }
+    }
+
+    /**
+     * Set and initialize the view elements.
+     */
+    private void initializeView() {
+        findViewById(R.id.notify_me).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, ChatHeadService.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+
+            //Check if the permission is granted or not.
+            // Settings activity never returns proper value so instead check with following method
+            if (Settings.canDrawOverlays(this)) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
